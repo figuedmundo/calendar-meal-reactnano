454cb312c19a3a24f07aed8a74e3691fdf27b2d5
Edmundo Figueroa <figuedmundo@gmail.com>
2018-04-25 01:40:07 -0400

    worrking

--- src/index.js
+++ src/index.js
@@ -3,6 +3,33 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, applyMiddleware, compose } from 'redux'
+import reducer from './reducers'
+import { Provider } from 'react-redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const logger = store => next => action => {
+  console.group(action.type)
+  console.info('dispatching', action)
+  let result =  next(action)
+  console.log('next state', store.getState())
+  console.groupEnd(action.type)
+  return result
+}
+
+// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSIONS_COMPOSE__ || compose
+
+const store = createStore(
+  reducer,
+  // applyMiddleware(logger)
+  composeEnhancers(
+    applyMiddleware(logger)
+  )
+)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, document.getElementById('root'));
+  
 registerServiceWorker();
